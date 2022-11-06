67b3fb14a65e6d9c8c0f9399148d38c9e7f99041
Trex <trex@piranaking.com>
2022-11-06 18:37:57 +0100

    Initial commit

---
+++ config.bat
@@ -0,0 +1,8 @@
+set UE4PATH=C:\Program Files\Epic Games\UE_4.26
+set GAMEPATH=E:\STEAM\steamapps\common\ENDER LILIES\EnderLilies
+set MODNAME=DebugMod
+
+
+set PROJPATH=%~dp0
+set PROJPATH=%PROJPATH:~0,-1%
+for %%I in (.) do set PROJNAME=%%~nxI
---
+++ package.bat
@@ -0,0 +1,21 @@
+call config.bat
+
+call "%UE4PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun^
+ -nocompileeditor -SkipCookingEditorContent -installed -nop4^
+ -project="%PROJPATH%\%PROJNAME%.uproject"^
+ -prereqs -nodebuginfo -manifests -targetplatform=Win64 -clientconfig=Development -utf8output -iostore^
+ -cook -stage -pak -package^
+ -ddc=InstalledDerivedDataBackendGraph^
+ 
+if exist "%PROJPATH%\%MODNAME%\" RD /S /Q "%PROJPATH%\%MODNAME%"
+MKDIR "%PROJPATH%\%MODNAME%"
+
+XCOPY "%PROJPATH%\Saved\StagedBuilds\WindowsNoEditor\%PROJNAME%\Content\Paks\*chunk1*" "%PROJPATH%\%MODNAME%\."
+
+For /R "%PROJPATH%\%MODNAME%\" %%# in ("*.*") Do (
+  ren "%%#" "%MODNAME%_0_P%%~x#"
+)
+
+if exist "%GAMEPATH%\Content\Paks\%MODNAME%\" RD /S /Q "%GAMEPATH%\Content\Paks\%MODNAME%%"
+
+XCOPY /Y /C /I /E "%PROJPATH%\%MODNAME%" "%GAMEPATH%\Content\Paks\%MODNAME%"
